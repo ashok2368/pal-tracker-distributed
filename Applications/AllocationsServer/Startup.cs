40ee4a41e49a545f39f8e16191d3de10a6dc52bc
ashok2368 <ashok2368@gmail.com>
2020-06-25 04:40:18 +0000

    service discovery updated

--- Applications/AllocationsServer/Startup.cs
+++ Applications/AllocationsServer/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Steeltoe.CloudFoundry.Connector.MySql.EFCore;
 using Steeltoe.Management.CloudFoundry;
+using Steeltoe.Discovery.Client;
+using Steeltoe.Common.Discovery;
 
 namespace AllocationsServer
 {
@@ -30,10 +32,13 @@ namespace AllocationsServer
 
             services.AddScoped<IAllocationDataGateway, AllocationDataGateway>();
             services.AddDbContext<AllocationContext>(options => options.UseMySql(Configuration));
+
+            services.AddDiscoveryClient(Configuration);
             
             services.AddSingleton<IProjectClient>(sp =>
             {
-                var httpClient = new HttpClient
+                var handler = new DiscoveryHttpClientHandler(sp.GetService<IDiscoveryClient>());
+                var httpClient = new HttpClient(handler, false)
                 {
                     BaseAddress = new Uri(Configuration.GetValue<string>("REGISTRATION_SERVER_ENDPOINT"))
                 };
@@ -56,6 +61,8 @@ namespace AllocationsServer
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseDiscoveryClient();
 
             app.UseEndpoints(endpoints =>
             {
